1b0924700e96dfb714a58985aaa08c98353ec1b3
Carlos Lukeni <carloslukeni110@gmail.com>
2025-05-19 21:31:19 +0100

    m

---
+++ src/app/ui/screens/index.tsx
@@ -0,0 +1,33 @@
+import { Box } from "@chakra-ui/react";
+
+/* FUNDO: #F4F5F7
+TEXTO P: #1E1E2F
+TEXTO S: #6B7280
+PRIMÁRIA BTNS: #6C63FF
+SECUNÁRIA: #A78BFA
+CARD/BG: #FFFFFF ou #EDEDF0
+SUCESSO: #10B981
+ERRO: #EF4444
+AVISO:FBBF24 */
+
+export default function Login(){
+      return(
+            <Box
+                  h="100vh"
+                  bg="#F4F5F7"
+                  display="flex"
+                  justifyContent="center"
+                  alignItems="center"
+            >
+                  <Box
+                        bg="white"
+                        w="400px"
+                        h="400px"
+                  >
+                        o
+
+                  </Box>
+                  
+            </Box>
+      )
+}
